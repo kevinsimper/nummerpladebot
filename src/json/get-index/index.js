36b144415085eb3833114de203d0dceb2df4f2a9
Sam Thomson <samt@samt.st>
2017-08-09 23:32:44 +0200

    started verificaiton 'route'

--- src/json/get-index/index.js
+++ src/json/get-index/index.js
@@ -1,9 +1,36 @@
 var arc = require('@architect/functions')
 
 function route(req, res) {
+
   console.log(JSON.stringify(req, null, 2))
+
+  let expectedToken = 'sam'
+  let resp = 'hej'
+
+  // are we handling a verification request
+  if (
+    req.query['hub.mode'],
+    req.query['hub.challenge'],
+    req.query['hub.verify_token']
+  ) {
+    if (req.query['hub.verify_token'] === expectedToken) {
+      // console.log(req.query['hub.challenge'])
+      // res(req.query['hub.challenge'])
+      res({
+        json: req.query['hub.challenge']
+      })
+      return
+    } else {
+      resp = 'invalid params..'
+    }
+  } else { 
+    resp = 'no params'
+  }
+
   res({
-    json: {msg:`hello world`}
+    json: {
+      'resp': resp
+    }
   })
 }
 

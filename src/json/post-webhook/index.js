3d05ddfe46ab181a7e5d775abe134cd6f2fdfeeb
Kevin Simper <kevin.simper@gmail.com>
2017-08-15 18:51:52 +0200

    Move out send api

--- src/json/post-webhook/index.js
+++ src/json/post-webhook/index.js
@@ -1,28 +1,5 @@
 var arc = require('@architect/functions')
-var request = require('request')
-const PAGE_ACCESS_TOKEN = (process.env.NODE_ENV === 'production') ? process.env.PAGE_ACCESS_TOKEN : 'development'
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
-}
+var callSendAPI = require('./send-api')
 
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
---
+++ src/json/post-webhook/send-api.js
@@ -0,0 +1,26 @@
+var request = require('request')
+const PAGE_ACCESS_TOKEN = (process.env.NODE_ENV === 'production') ? process.env.PAGE_ACCESS_TOKEN : 'development'
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+module.exports = callSendAPI

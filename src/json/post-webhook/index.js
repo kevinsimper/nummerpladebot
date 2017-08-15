0614a00e92456d284c7590a47808c67aa2c8d055
Kevin Simper <kevin.simper@gmail.com>
2017-08-15 18:10:51 +0200

    Add test for receiving webhook

--- src/json/post-webhook/index.js
+++ src/json/post-webhook/index.js
@@ -1,9 +1,35 @@
 var arc = require('@architect/functions')
 
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log('Message data: ', event.message)
+}
+
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
+  const data = req.body
+
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id
+      var timeOfEvent = entry.time
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event)
+        } else {
+          console.log('Webhook received unknown event: ', event)
+        }
+      })
+    })
+  }
+
   res({
-    json: {hello:'world'}
+    json: {
+      status: 'received'
+    }
   })
 }
 
---
+++ tests/post-webhook.test.js
@@ -0,0 +1,50 @@
+let arc = require('@architect/workflows')
+let http = require('tiny-json-http')
+
+let server
+test('that we can start local server', done => {
+  expect.assertions(1)
+  server = arc.sandbox.http.start(() => {
+    expect(true).toBe(true)
+    done()
+  })
+})
+
+test('verify webhook', done => {
+  let examplepost = {
+    object: 'page',
+    entry: [
+      {
+        id: '229557634234995',
+        time: 1502812631625,
+        messaging: [
+          {
+            sender: {
+              id: '1384083618356159'
+            },
+            recipient: {
+              id: '229557634234995'
+            },
+            timestamp: 1502812628024,
+            message: {
+              mid: 'mid.$cAADQyDzsOYFkF-IsOFd5pzj0d7Gs',
+              seq: 64708,
+              text: 'test'
+            }
+          }
+        ]
+      }
+    ]
+  }
+  http.post(
+    { url: 'http://localhost:3333/webhook', data: examplepost },
+    (err, res) => {
+      expect(res.body).toEqual({"status": "received"})
+      done()
+    }
+  )
+})
+
+test('server close', () => {
+  server.close()
+})
